c1b85f511bea76b93e66ac77e5d672fe70dbc2cc
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-07 18:37:22 +0800

    EX12

---
+++ ACAD/Y1/S1/MINOR/EX/EX12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int magic, guess;
+    magic = rand();
+    cout << "Enter your guess: \n";
+    cin >> guess;
+    if(guess == magic){
+        cout << "You're right. \n";
+        cout << magic << " is the magic number.";
+    }
+    else {
+        if(guess > magic)
+            cout << "Your guess is higher.\n";
+        else
+            cout << "Your guess is lower.\n";
+    }
+    return 0;
+}
