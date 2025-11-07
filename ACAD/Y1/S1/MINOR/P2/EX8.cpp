a4eabd131ef0e2ac6eb17c0dd07e280dafe87a9b
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-07 18:28:38 +0800

    EX8

---
+++ ACAD/Y1/S1/MINOR/P2/EX8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+// Tubig marka ni shantidope
+
+int main()
+{
+    for (int j = 0; j <= 8; j++) {
+        if (j == 4) continue;
+        cout << j;
+    }
+    cout << endl;
+    return 0;
+}
