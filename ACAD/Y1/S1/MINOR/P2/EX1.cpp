2a491008ab3093a16e7dab052f288d5ba05e625a
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-07 18:24:02 +0800

    INITIAL

---
+++ ACAD/Y1/S1/MINOR/P2/EX1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+// Tubig marka ni shantidope
+
+int main()
+{
+    for (int num = 3; num >= 1; --num)
+        cout << num << " ";
+    return 0;
+}
