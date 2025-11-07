6c69c420712fcd47e09362f8bb47868d5958bc41
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-07 18:27:40 +0800

    EX6

---
+++ ACAD/Y1/S1/MINOR/P2/EX6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+// Tubig marka ni shantidope
+
+int main() {
+    int n;
+    do {
+        cout << "Enter number(0 to end): ";
+        cin >> n;
+        cout << "You entered: " << n << "\n";
+    }
+    while (n != 0);
+    return 0;
+}
