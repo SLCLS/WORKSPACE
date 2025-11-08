fdc65ccf784fd93450177dd4df695bd8aaa511bc
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-08 23:24:38 +0800

    STANDARD VALID INT

--- ACAD/Y1/S1/OTHERS/STANDARD.cpp
+++ ACAD/Y1/S1/OTHERS/STANDARD.cpp
@@ -9,7 +9,18 @@ using namespace std;
 
 int main()
 {
+    int number, number_check;
 
+    cout << "ENTER AN INTEGER: \n";
+    cin >> number;
+
+    number_check = int(number);
+
+    if (number_check != number)
+    {
+        cout << "NOT AN INTEGER!";
+        return 1;
+    }
     
     return 0;
 }
