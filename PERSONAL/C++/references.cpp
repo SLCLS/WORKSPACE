9f7002d1fe31997e805c5970340dd968284e625d
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-10 12:22:49 +0800

    References.cpp Error Suppression

--- PERSONAL/C++/references.cpp
+++ PERSONAL/C++/references.cpp
@@ -1,3 +1,31 @@
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : all)
+#endif
+#ifdef GNUC  // For GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+#ifdef clang
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wpedantic"
+#endif
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+#ifdef GNUC
+#pragma GCC diagnostic pop
+#endif
+#ifdef clang
+#pragma clang diagnostic pop
+#endif
+
+// START HERE, DISREGARD THE ERROR SUPPRESSION ABOVE.
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
