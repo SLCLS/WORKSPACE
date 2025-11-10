612ad1290f92af825ba4b693d96f9a2adc6402f3
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2025-11-10 12:30:25 +0800

    Shorter C++ Ref Suppression

--- PERSONAL/C++/references.cpp
+++ PERSONAL/C++/references.cpp
@@ -1,27 +1,10 @@
 #ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : all)
-#endif
-#ifdef GNUC  // For GCC
+#pragma warning(push, 0)
+#else
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wpedantic"
-#endif
-#ifdef clang
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wall"
-#pragma clang diagnostic ignored "-Wextra"
-#pragma clang diagnostic ignored "-Wpedantic"
-#endif
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-#ifdef GNUC
-#pragma GCC diagnostic pop
-#endif
-#ifdef clang
-#pragma clang diagnostic pop
 #endif
 
 // START HERE, DISREGARD THE ERROR SUPPRESSION ABOVE.
@@ -36,3 +19,13 @@
 using namespace std;
 
 // THIS FILE IS BEING USED AS A REFERENCE FOR C++ SYNTAX AND CONCEPTS
+
+
+
+// END HERE, DISREGARD THE ERROR SUPPRESSION BELOW.
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#else
+#pragma GCC diagnostic pop
+#endif
